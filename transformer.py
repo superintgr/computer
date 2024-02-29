bf7ed3e556cf464ef64792fb504c60ba0543da6c
superintgr <155528797+superintgr@users.noreply.github.com>
2024-02-28 22:04:40 -0500

    Create transformer.py

---
+++ transformer.py
@@ -0,0 +1,12 @@
+class Transformer(object):
+    """
+    Transformer circuit model.
+
+    primary : input gate
+    secondary : output gate
+    source : inductor node
+    target : resistor node
+    transfer : ratio between impedences
+    """
+   def __init__(self):
+       pass

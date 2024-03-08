e28daae893e2444aa1fa7c83bf0124847e845ef7
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-08 12:44:29 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+"""
+1. what possible kinds of error could be found?
+2. how to position the nodes to capture enough data?
+3. define the pipeline for potential error scene and what are they?
 
+
+[plan out the basic form of interaction from end-to-end]
+-1. find the available handlers for all changes to memory buffer
+-0. bring top k handlers to the queue for buffer elements
++0. 
+"""
 
 class Test:
     ..

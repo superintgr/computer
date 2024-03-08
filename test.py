d80b518d4e746cd66166802d695df496892feb16
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-08 12:20:12 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+class Test:
+    ..
+
+
+func = some_parametric_function(prep_batch)
+out, in, err = func(next(cases))
+corr, out = gunc(err, in)
+
+rational = []
+
+for e in cases:
+    rational.appendfout / in * err / out * corr / in * out / err)

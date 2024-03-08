afa29b409f151afe2997b602aafda8614739a8e9
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-08 16:44:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -34,9 +34,93 @@ if change:
                 break
         change += q
     
+[bringing top k agents]
+ we have to define what kind of agency the system of agents and others will be maintaining.
+ by maintain, I am referring to the core principles that would apply to every agents and all interactions.
+ the interactions are between agents and everything else required for setting up the system protocols
+
+ ## key points:
+ 1. agency and principled organization,
+ 2. system-wide flow and effect of organization,
+ 3. maintainence and [error corrections, this is new]
+
+ [model of communication]
+  there will be layered structures:
+  . operating system tasks and retaining functional previleges,
+  . basic computation substrate and memory usage,
+  . scanning external inputs and preprocessing of bytes,
+  . delivery mechanism and logging tools
+  . parametrization and service discovery
+  . broadcasting language and automatic publishing routines
+  . error detection and redundancy
+  
+
+[operating system tasks and administrative previleges]
+    import subprocess
+    import node, antinode
+    
+    environments = {}
+    workers = {}
+    presets = {}
+
+    regular = node.load_substrate(__file__)
+    irregular = antinode.load_substrate(__file__)
+
+    request = lambda environment, worker, preset=None: regular.prepare(environment, worker, preset)
+    terminate = lambda process, callback: irregular.prepare(process, callback)
 
 
+## comment ##
+
+  all the subprocess related tasks could be handled through node/antinode protocols.
+
+[basic computation substrate and memory usage]
+
+    import node, antinode
+    import os
+    import psutils
+    
+    storage = lambda size, _node, _antinode: node.allocate(size, source=_node, target=_antinode)
+
+[scanning external inputs and processing bytes]
+    import node, antinode
+
+    def sample(path, scanner, feedforward):
+        substrate = node.unsqueeze(path)
+        scope = antinode.squeeze(substrate, scanner)
+        return feedforward(scope)
+
+    def communicate(address, callback, shape, language):
+        buffer = node.stream(address, shape)
+        batch, decoder = antinode.encode(buffer, language)
+        receipt = callback(batch)
+        status = buffer(decoder(receipt)):
+        buffer.close()
+        return status
+
+
+
+
+        
+"""Here is an idea:
+
+[this is what i have at non-programming setting]
+    . pattern generator and step sequencer
+    . piano roll and graph features
+    . playlist for placing tracks
+    . automation clips
+    . mixing console with N channels
+    . channel functions and i/o
+    . connection matrix and routing bus
+    . slicing/quantizing/modifying etc.
+
+[this is what i want]
+    . describe computation in the playlist
+    . compile audio runtime with N parallel processes
+    . map dynamical parameters from track space to symbolic programming api
+    . many more than haven't been discovered
 """
+
 
 
 class Test:

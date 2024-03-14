64724a3a47a8ede73c34fcb6a316d7f4bea0dffa
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-13 22:05:11 -0400

    Create message_passing.py

---
+++ message_passing.py
@@ -0,0 +1,17 @@
+"""
+**Message Passing Module**
+
+. messages are sent by one object to another object
+. sender object puts message data to a common object
+. common object puts message data to receiver object
+. receiver-sender connection is via the common object
+
+- start with the common object
+- set the attributes contained in the properties defined for the common mode messaging
+- create a pair of sender-receiver objects
+- if message from any object to common object is possible, then another message for collecting an information variable as message from the associated other object
+
+[object1 and object2 are both sender and receiver]
+[object3 can receive message from both objects]
+
+"""

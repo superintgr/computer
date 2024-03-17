c7a27f9f18c8b792cde40a09ec3d1e42b234150c
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-17 06:34:59 -0400

    Update message_passing.py

--- message_passing.py
+++ message_passing.py
@@ -15,3 +15,73 @@
 [object3 can receive message from both objects]
 
 """
+
+
+
+modules = [information, computation, measurement, construction]
+
+class Construction:
+    output : state dict
+    input  : state repr
+    task   : input -> output
+
+"""
+***Construction tasks***
+
+Tasks are defiened as:
+    required state of the substrate  : input attribute
+    output property of the substrate : target attribute
+
+    where a constructor would transform the input attributes (if found in that state)
+    to the desired final state while remaining capable for the task again.
+
+If the input attributes are not readily available, the construction of that substrate
+. with the desired properties or turning the existing one to converge onto the given
+. properties is a possible task.
+
+Chaining tasks look like:
+    [serial composition] is the task performing on one substrate, depending on the
+    . output of another one while doing one after another in succession.
+
+    [parallel composition] is the task of performing in two disjoint substrates
+    . in parallel and combining their individual transformations together using
+    . additive operation inside another substrate.
+
+Transformation that remains valid:
+    [transposition] *** elaborate on that more!
+"""
+
+
+class Information:
+    generic  : random samples
+    basis    : selected samples
+    task     : basis, generic   -> basis, basis
+
+class Computation:
+    operator : permutation
+    iterator : variable
+    requires : (at least) two state
+    computes : unitary state
+    task     : union of all variable pairs in substrate where (element -> permutation of element) for all variable in unit pair
+
+"""
+**Computation**
+
+Given some substrate such as memory elements from a volume contained in storage disk, the computation is the permutation operation
+. performed on each memory elements of the substrate such their unity is possible whenever one of the ways of variable elements in
+. disk volume could change into having a distinct other ways, with no less than two distinguishable attributes of the elements in
+. substrate being present, one of them commutes with the other while the computation (being reversible) asserts that their dynamical
+. variables map back to one of the other elements in that same set.
+
+The union operation is there to ensure that the computation variables that participated in the transformation keeps in existence
+. for subsequent transformations.
+
+
+**Information**
+
+It is a property of a substrate where it is able to copy certain attributes from interacting with other objects while retaining the
+. counterfactual information parameters become useful.
+
+For some substrate that is reversible information processing media could use generic naturally occuring resources to construct new
+. media for computation or any other information processing task.
+"""
